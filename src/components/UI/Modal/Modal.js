68ef35fffdaf4291fc691c788cb67b919b5a0cf9
Marcin Puc <marcinpuc@MacBook-Pro-Marcin.local>
2020-10-08 18:02:46 +0200

    Dodanie tła oraz umozliwienie zrezygnowania z zamowienia

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import styles from './Modal.module.css';
+import Aux from '../../../hoc/Aux';
+import Backdrop from '../Backdrop/Backdrop';
 
 const modal = (props) =>(
-    <div className={styles.Modal}
-    style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-    }}>
-        {props.children}
-    </div>
+    <Aux>
+        <Backdrop show={props.show} clicked={props.modalClosed}></Backdrop>
+        <div className={styles.Modal}
+        style={{
+            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: props.show ? '1' : '0'
+        }}>
+            {props.children}
+        </div>
+    </Aux>
 )
 
 export default modal;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,6 +27,9 @@ class BurgerBuilder extends Component{
     purchaseHandler = () => {
         this.setState({purchasing:true});
     }
+    purchaseCancelHandler = () =>{
+        this.setState({purchasing: false})
+    }
     updatePurchaseState (ingredients){
         const sum = Object.keys(ingredients)
         .map(igKey =>{
@@ -76,7 +79,7 @@ class BurgerBuilder extends Component{
         }
         return(
             <Aux>
-                <Modal show={this.state.purchasing}>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary ingredients={this.state.ingredients}></OrderSummary>
                 </Modal>
                 <Burger ingredients={this.state.ingredients}></Burger>
